2bcf3163f449c4888e630297c48d10e2e42f7157
nicob1800 <Nicolas.b18@hotmail.com>
2023-08-22 12:48:23 -0400

    changes

--- File_organizer.py
+++ File_organizer.py
@@ -6,53 +6,26 @@ Fname = input("File name: ")
 Dfolder = input("New folder name: ")
 Nname = input("New name: ")
 
-letters = ['A', 'B', 'C', 'D']
-numbers = ['01', '02', '03', '04']
+# Create the destination folder if it doesn't exist
 path = os.path.join(Fpath, Dfolder)
+os.makedirs(path, exist_ok=True)
 
-try:
-    os.rmdir(path)
-except:
-    pass
-
-#END OF COPY METHOD
-
-
-#print(source_path)
-os.mkdir(path)
-
-print(path)
-
-
-#copy_source = 
-
-
+# Find occurrences of the specified file name in directories
 occurrences = 0
-namenum = 3
-typed = f"0{namenum}"
-
-
 for root, dirs, files in os.walk(Fpath):
     if Fname in files:
         occurrences += 1
-
-i = occurrences
-
-if i >= 0:
-
-    source_path = f"{Fpath}\Timepoint_{i}\{Fname}"
-    destination_path = f"{Fpath}\{Dfolder}"
-
-    try:
-        shutil.copy(source_path, destination_path)
-        os.rename(Fname, f"{Nname}{i}")
-
-    except shutil.SameFileError:
-        print("Same file error")
-    
-    i -= 1
+        i = occurrences
+        source_path = os.path.join(root, Fname)
+        destination_path = os.path.join(path, f"{Nname}_{i}.tif")
         
-
+        try:
+            shutil.copy(source_path, destination_path)
+            print(f"File '{Fname}' copied to '{destination_path}'")
+        except shutil.SameFileError:
+            print("Same file error")
+        except FileNotFoundError:
+            print(f"File '{Fname}' not found in '{root}'")
+        
+# Print the total number of occurrences
 print(f"The file '{Fname}' was found {occurrences} times.")
-
-

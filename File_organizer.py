6d442e5702c77c32e559636e93df3549510d046a
nicob1800 <Nicolas.b18@hotmail.com>
2023-08-23 12:17:44 -0400

    It works, do not change, only branch

--- File_organizer.py
+++ File_organizer.py
@@ -45,6 +45,9 @@ def org_files():
 
 
 def make_video():
+    global path
+    videopath = input("Path to the video frames: ") #comment out later
+    name = input("Name of the video: ")
     img_array = []
     for filename in glob.glob(f"{path}/*.tif"):
         img = cv2.imread(filename)
@@ -52,15 +55,16 @@ def make_video():
         size = (width,height)
         img_array.append(img)
 
-    out = cv2.VideoWriter('project1.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 2, size)
+    out = cv2.VideoWriter(f"{name}.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 2, size)
  
     for i in range(len(img_array)):
         out.write(img_array[i])
-        out.release()
+    
+    out.release()
+    print(f"\033[1;31;40m{out}\033[m")
 
 org_files()
 print("Files organized")
-
 
 make_video()
 
